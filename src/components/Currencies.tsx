872fa7153e2cc933f9639e32da28d1e9601038e6
Vikt0r <home@home.ru>
2022-05-19 22:21:18 +0300

    Save quotes in the temporary file

--- src/components/Currencies.tsx
+++ src/components/Currencies.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { observer } from "mobx-react-lite";
+
+import { StoreContext } from "../index";
 
 function Currencies() {
+  const rootStore = useContext(StoreContext);
+  const converterStore = rootStore.converter;
+
   return <h2>Currencies</h2>;
 }
 
--- src/mobx/converter/Converter.ts
+++ src/mobx/converter/Converter.ts
@@ -1,18 +1,29 @@
-export default class Converter {
-  private _apiKey: string;
-  constructor() {
-    this._apiKey = process.env.REACT_APP_API_KEY!;
+import { quotes_rub, quotes_usd } from "./quotes";
 
-    fetch(
-      "https://api.apilayer.com/currency_data/live?base=USD&symbols=EUR,GBP",
-      {
-        method: "GET",
-        headers: {
-          apikey: this._apiKey,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+interface Quotes {
+  [index: string]: number;
+}
+
+export default class Converter {
+  private _apiKey: string = process.env.REACT_APP_API_KEY!;
+  private _base: string = "USD";
+  private _quotes: Quotes;
+
+  constructor() {
+    //this._apiKey = process.env.REACT_APP_API_KEY!;
+    //this._base = 'RUB';
+
+    // fetch(
+    //   "https://api.apilayer.com/currency_data/live?source=RUB&currencies=",
+    //   {
+    //     method: "GET",
+    //     headers: {
+    //       apikey: this._apiKey,
+    //     },
+    //   }
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data));
+    this._quotes = quotes_usd;
   }
 }
